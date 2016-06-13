7b9e08490ae7c73390e25fe5ec342ff981e62813
srikanthprem <srikanthprem@techaspect.com>
2016-06-13 15:54:45 +0530

    Missing models update

---
+++ TAMVC/Models/MyAccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TAMVC.Models.DBModels;
+
+namespace TAMVC.Models
+{
+    public class MyAccountModel
+    {
+        public IEnumerable<TAC_Classified> lst { get; set; }
+
+        public Pager Pager { get; set; }
+    }
+}
